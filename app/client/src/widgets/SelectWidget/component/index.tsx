b20ac1a131a62e0c2338765b2737214ffe36ddbe
balajisoundar <balaji@appsmith.com>
2022-03-28 16:31:44 +0530

    select columnType

--- app/client/src/widgets/SelectWidget/component/index.tsx
+++ app/client/src/widgets/SelectWidget/component/index.tsx
@@ -276,6 +276,7 @@ class SelectComponent extends React.Component<
 
     return (
       <DropdownContainer
+        className={this.props.className}
         compactMode={compactMode}
         data-testid="select-container"
         labelPosition={labelPosition}
@@ -364,6 +365,7 @@ class SelectComponent extends React.Component<
 }
 
 export interface SelectComponentProps extends ComponentProps {
+  className?: string;
   disabled?: boolean;
   onOptionSelected: (optionSelected: DropdownOption) => void;
   placeholder?: string;
@@ -389,6 +391,7 @@ export interface SelectComponentProps extends ComponentProps {
   value?: string;
   label?: string;
   filterText?: string;
+  hideCloseIcon?: boolean;
 }
 
 export default React.memo(SelectComponent);
